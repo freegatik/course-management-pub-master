bcaab105a63eb61dd7d0d2ad8fb3bb3e2de33f54
freegatik <anton.soloviev2016@gmail.com>
2025-12-12 01:49:43 +0300

    Исправление уязвимостей OWASP Top 10: добавлены комментарии ко всем исправлениям, улучшена проверка в workflow

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- Инициализация демо-данных
--- Пользователи
+-- Пользователи (пароли захешированы с помощью BCrypt, оригинальный пароль: "password")
 INSERT INTO USERS (id, username, password, role)
-VALUES (1, 'teacher', 'password', 'TEACHER');
+VALUES (1, 'teacher', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'TEACHER');
 INSERT INTO USERS (id, username, password, role)
-VALUES (2, 'student', 'password', 'STUDENT');
+VALUES (2, 'student', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'STUDENT');
 ALTER TABLE USERS ALTER COLUMN ID RESTART WITH 3;
 
 -- Студенты
